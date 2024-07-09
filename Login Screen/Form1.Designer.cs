d99ace3fa9755ed987c86e5a3fbfc0497dd48695
OsborneSebuabe <ojsebuabe@st.ug.edu.gh>
2024-07-09 23:12:31 +0000

    Login Screen

---
+++ Login Screen/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Login_Screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsername = new TextBox();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            btnLogin = new Button();
+            txtPassword = new MaskedTextBox();
+            SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(154, 103);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 0;
+            txtUsername.TextChanged += textBox1_TextChanged;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUsername.Location = new Point(49, 110);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(80, 20);
+            lblUsername.TabIndex = 2;
+            lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPassword.Location = new Point(475, 110);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(76, 20);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.Image = Properties.Resources.login;
+            btnLogin.ImageAlign = ContentAlignment.MiddleLeft;
+            btnLogin.Location = new Point(350, 289);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(86, 29);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.TextAlign = ContentAlignment.MiddleRight;
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(600, 103);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtPassword);
+            Controls.Add(btnLogin);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(txtUsername);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsername;
+        private Label lblUsername;
+        private Label lblPassword;
+        private Button btnLogin;
+        private MaskedTextBox txtPassword;
+    }
+}
---
+++ Login Screen/Form1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Windows.Forms;
+
+namespace Login_Screen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Info");
+        }
+    }
+}
